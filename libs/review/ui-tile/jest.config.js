02a85850e9fa11e3533ff2febc31c4dd41258682
Tuan Dang <dangviettuan1508@gmail.com>
2020-03-25 00:59:39 +0700

    Remove unused code

--- libs/review/ui-tile/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'review-ui-tile',
-  preset: '../../../jest.config.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/review/ui-tile'
-};
